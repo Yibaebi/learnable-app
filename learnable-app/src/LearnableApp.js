62ebbaef36c9c0235bde43ad0aab277263cac548
Yibaebi Elliot <59321440+Yibaebi@users.noreply.github.com>
2020-10-19 01:34:58 +0100

    Built responsive mobile view for landing page

--- learnable-app/src/LearnableApp.js
+++ learnable-app/src/LearnableApp.js
@@ -1,33 +1,36 @@
-import React from 'react';
-// import Footer from "./Components/Footer/Footer";
-import Footer from "./Components/LearnablePage-Subscribe/Subscribe";
-import LandingNavBar from './Components/NavBars/LandingPage';
-import LandingPage from "./Components/Pages/Landing Page/LandingPage";
+import React from "react";
+
+import LandingNavBar from "./Components/NavBars/LandingPage";
 import LearnableHome from "./Components/Pages/Learnable Page/Home";
 import SoftwareDeveloper from "./Components/Pages/Learnable Page/SoftwareDeveloper";
 import ProductDesigner from "./Components/Pages/Learnable Page/ProductDesigner";
 import Apply from "./Components/Pages/Learnable Page/Apply";
 import Journal from "./Components/Pages/Learnable Page/Journal";
 import Faq from "./Components/Pages/Learnable Page/Faq";
+import LandingPage from "./Components/Pages/Landing Page/LandingPage";
 
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function LearnableApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Footer /> */}
-        {/* <LandingNavBar /> */}
-        <LandingPage />
-        {/* <LearnableHome /> */}
-        {/* <SoftwareDeveloper /> */}
-        {/* <ProductDesigner /> */}
-        {/* <Apply /> */}
-        {/* <Journal /> */}
-
-        {/* <Faq /> */}
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <LandingNavBar />
+          <div>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/Learnablehome" component={LearnableHome} />
+              <Route path="/Software" component={SoftwareDeveloper} />
+              <Route path="/Product" component={ProductDesigner} />
+              <Route path="/Apply" component={Apply} />
+              <Route path="/Journal" component={Journal} />
+              <Route path="/Faq" component={Faq} />
+            </Switch>
+          </div>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
