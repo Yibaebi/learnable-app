ff54a64e86afafa5b18bfd178f561d4dc57a9ba2
Yibaebi Elliot <59321440+Yibaebi@users.noreply.github.com>
2020-10-19 01:42:14 +0100

    first production build uploaded

---
+++ build/precache-manifest.e05d28b14edcbed0a998cd95122cee7b.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8d681f5aa79f6bdbf3c242e20ac0bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc9cf852d1de7c01b051",
+    "url": "/static/css/main.dd1cc64e.chunk.css"
+  },
+  {
+    "revision": "ca359dadf8cc5ba63235",
+    "url": "/static/js/2.0f39f06f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0f39f06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9cf852d1de7c01b051",
+    "url": "/static/js/main.5790aa80.chunk.js"
+  },
+  {
+    "revision": "cdae9ef4db1d798f314c",
+    "url": "/static/js/runtime-main.640a7c6f.js"
+  },
+  {
+    "revision": "13846e83a3cd995412abba95855a22d0",
+    "url": "/static/media/Ana 1.13846e83.png"
+  },
+  {
+    "revision": "77d5a05cbeb73a918ecf6ed192a412f2",
+    "url": "/static/media/Cutout 1.77d5a05c.png"
+  },
+  {
+    "revision": "899fc21d821ad414b2ea470200422823",
+    "url": "/static/media/FAQ (First Image).899fc21d.png"
+  },
+  {
+    "revision": "0ee7064babc5c983766988d10c54ff18",
+    "url": "/static/media/Footer (Learnable Page).0ee7064b.png"
+  },
+  {
+    "revision": "be48750ce25815d0146cb309da398d7d",
+    "url": "/static/media/GenesysLogo.be48750c.jpg"
+  },
+  {
+    "revision": "c35f743624ab19698f59e2ae1394519b",
+    "url": "/static/media/Go back to Top.c35f7436.svg"
+  },
+  {
+    "revision": "597ef22fa2093f79d1160c4b78cd4273",
+    "url": "/static/media/IMG_0491-2@2x-400x359 1.597ef22f.png"
+  },
+  {
+    "revision": "2b4b3cd4661fcbbbc31e16993824683e",
+    "url": "/static/media/IMG_2087@2x-400x359 1.2b4b3cd4.png"
+  },
+  {
+    "revision": "1ac7543f3d93cf1f37481601378623ad",
+    "url": "/static/media/IMG_4354@2x-400x359 1.1ac7543f.png"
+  },
+  {
+    "revision": "1e0148b1d35d7301622dac5f4ad7b029",
+    "url": "/static/media/IMG_4403@2x-400x359 1.1e0148b1.png"
+  },
+  {
+    "revision": "ceb54b489f15d180fc6d223bde3c7ffd",
+    "url": "/static/media/IMG_4604@2x-400x359 1.ceb54b48.png"
+  },
+  {
+    "revision": "0cae580fb40336a8e1595872b2d07c3c",
+    "url": "/static/media/Journal Page (First Image).0cae580f.png"
+  },
+  {
+    "revision": "0639b486111848544703b105dc16a25e",
+    "url": "/static/media/Photo 1.0639b486.jpg"
+  },
+  {
+    "revision": "6a20a61ffcc29c798b0a9b8a8ba80978",
+    "url": "/static/media/Photo 2.6a20a61f.jpg"
+  },
+  {
+    "revision": "fe15a1e44e89a88466e48e88a38ad891",
+    "url": "/static/media/Photo 3.fe15a1e4.jpg"
+  },
+  {
+    "revision": "7849b6a640d9bb5543d98ad337c65b48",
+    "url": "/static/media/Photo-1.7849b6a6.png"
+  },
+  {
+    "revision": "5c2c1ed6eec0eb521013c35cfd7625d3",
+    "url": "/static/media/Photo-3.5c2c1ed6.png"
+  },
+  {
+    "revision": "0cbe5e2044ccffa51928e3596b0dcfaf",
+    "url": "/static/media/Photo.0cbe5e20.png"
+  },
+  {
+    "revision": "a379a90d5f89aed50923c211dd884824",
+    "url": "/static/media/Photo.a379a90d.png"
+  },
+  {
+    "revision": "ddeb479bfe4b1753a8eb34d04f6464f2",
+    "url": "/static/media/Photo.ddeb479b.png"
+  },
+  {
+    "revision": "ef3f5afb8a0118ab1b7c5b5701ddec51",
+    "url": "/static/media/Photo.ef3f5afb.png"
+  },
+  {
+    "revision": "ff4f6ddd0dccf820d9717b14ab5715d0",
+    "url": "/static/media/Photo.ff4f6ddd.jpg"
+  },
+  {
+    "revision": "59865986c224da0253c420ea29dd4cae",
+    "url": "/static/media/Rectangle 17.59865986.png"
+  },
+  {
+    "revision": "d899395d879e2e1a98772b22ac4e1e70",
+    "url": "/static/media/Rectangle 17.d899395d.png"
+  },
+  {
+    "revision": "0b8663572748a511ef634c69fa4b358d",
+    "url": "/static/media/Rectangle 18.0b866357.png"
+  },
+  {
+    "revision": "ebe548c3ffd37e578b193ca490f8073c",
+    "url": "/static/media/Rectangle 18.ebe548c3.png"
+  },
+  {
+    "revision": "5a61fbcd003be3ac5c5da483f1042d9b",
+    "url": "/static/media/Rectangle 19.5a61fbcd.png"
+  },
+  {
+    "revision": "fa70572330eeca9ecfec943f57f522e5",
+    "url": "/static/media/Rectangle 19.fa705723.png"
+  },
+  {
+    "revision": "796bd9083d1c7eabc2c49754d5a706c1",
+    "url": "/static/media/Rectangle 20.796bd908.png"
+  },
+  {
+    "revision": "c7a146a05558a1b3ddd5806bd43eb69a",
+    "url": "/static/media/Rectangle 20.c7a146a0.png"
+  },
+  {
+    "revision": "70c63c774b26d2b62dbbb5612168d7aa",
+    "url": "/static/media/Rectangle 21.70c63c77.png"
+  },
+  {
+    "revision": "8849f74967a9616f6e390e6053719b99",
+    "url": "/static/media/Rectangle 21.8849f749.png"
+  },
+  {
+    "revision": "31e96b63ce78d22f951a1147fc06136e",
+    "url": "/static/media/Rectangle 22.31e96b63.png"
+  },
+  {
+    "revision": "568883ba2ff4df89a9f97021c003aaa1",
+    "url": "/static/media/Rectangle 22.568883ba.png"
+  },
+  {
+    "revision": "4b5eb2ad565f1488a4e5988350d47aa1",
+    "url": "/static/media/Rectangle 23.4b5eb2ad.png"
+  },
+  {
+    "revision": "e92b3bbf433fd3ed881934adc66060f6",
+    "url": "/static/media/Rectangle 23.e92b3bbf.png"
+  },
+  {
+    "revision": "a49171dc6cf22f085ea08b4dd98663ce",
+    "url": "/static/media/Rectangle 24.a49171dc.png"
+  },
+  {
+    "revision": "d64c42836a488b15c25906c4c2a81f19",
+    "url": "/static/media/Rectangle 24.d64c4283.png"
+  },
+  {
+    "revision": "121b04e8e0dcd51d533e25f61bf879ee",
+    "url": "/static/media/Rectangle 25.121b04e8.png"
+  },
+  {
+    "revision": "935724f5aa4b6f2a5f183feae4287cd9",
+    "url": "/static/media/Rectangle 25.935724f5.png"
+  },
+  {
+    "revision": "d9ecaca89504fcb842b08a626fde5620",
+    "url": "/static/media/Rectangle 26.d9ecaca8.png"
+  },
+  {
+    "revision": "eef0480b752d00d24735e76ee1667540",
+    "url": "/static/media/Rectangle 26.eef0480b.png"
+  },
+  {
+    "revision": "2e3589a19424f0378f667559b1629ca1",
+    "url": "/static/media/Rectangle 27.2e3589a1.png"
+  },
+  {
+    "revision": "e97557eba21c9d03abb9f9f2edfa5e2f",
+    "url": "/static/media/Rectangle 27.e97557eb.png"
+  },
+  {
+    "revision": "ac0a40d56537eae94dc00f4c20a234de",
+    "url": "/static/media/Rectangle 28.ac0a40d5.png"
+  },
+  {
+    "revision": "da97dac798bc829a852c4af85025ec18",
+    "url": "/static/media/Rectangle 28.da97dac7.png"
+  },
+  {
+    "revision": "585d607e4e05e2add507b6078e470709",
+    "url": "/static/media/Rectangle 29.585d607e.png"
+  },
+  {
+    "revision": "d5b28ab6b53161b7dc6d1dd069530744",
+    "url": "/static/media/Rectangle 29.d5b28ab6.png"
+  },
+  {
+    "revision": "ce4d40acdb3ea1184be651573b84946c",
+    "url": "/static/media/Rectangle 30.ce4d40ac.png"
+  },
+  {
+    "revision": "ecb03df10bbf6a51c9193815642e6b23",
+    "url": "/static/media/Rectangle 30.ecb03df1.png"
+  },
+  {
+    "revision": "92d7f9b5de9b4cffd374ab85cfb3127e",
+    "url": "/static/media/Rectangle 31.92d7f9b5.png"
+  },
+  {
+    "revision": "ff5f0181e9cb178b0911945165f23aba",
+    "url": "/static/media/Rectangle 31.ff5f0181.png"
+  },
+  {
+    "revision": "03eec0711cb9d4766063f57f26756fea",
+    "url": "/static/media/Rectangle 32.03eec071.png"
+  },
+  {
+    "revision": "62a06edfdaef611cfa1a91ac0147b234",
+    "url": "/static/media/Rectangle 32.62a06edf.png"
+  },
+  {
+    "revision": "1496ed0415b356afd0df3f10d20edddd",
+    "url": "/static/media/Rectangle 36.1496ed04.png"
+  },
+  {
+    "revision": "c14d868ce99078d6e44056d219a92a00",
+    "url": "/static/media/Rectangle 36.c14d868c.jpg"
+  },
+  {
+    "revision": "0826b0b11dd00e9c8fa692c70681aef4",
+    "url": "/static/media/Rectangle 38-1.0826b0b1.jpg"
+  },
+  {
+    "revision": "04c1de5f37eb97b3f2f1141479195c53",
+    "url": "/static/media/Rectangle 38.04c1de5f.jpg"
+  },
+  {
+    "revision": "69b7cad7dc59e2e3849d146e0702c793",
+    "url": "/static/media/Rectangle 4.69b7cad7.png"
+  },
+  {
+    "revision": "bbe937456726d4f777ba15fdc9b5d576",
+    "url": "/static/media/Rectangle 42.bbe93745.png"
+  },
+  {
+    "revision": "f72590005a7ec78a43c73fc3e1b6af46",
+    "url": "/static/media/Rectangle 42.f7259000.jpg"
+  },
+  {
+    "revision": "1565ec0f25ee1cc20385f93e1b2db6d3",
+    "url": "/static/media/Vector.1565ec0f.svg"
+  },
+  {
+    "revision": "f968ef7951a1670b47f108928bd24a30",
+    "url": "/static/media/Vector3.f968ef79.svg"
+  },
+  {
+    "revision": "e0a026230a97be6a6a3e44880534f0c6",
+    "url": "/static/media/Vector4.e0a02623.svg"
+  },
+  {
+    "revision": "631acb0b59945a7518fca5ce3a2062fa",
+    "url": "/static/media/image 23-1.631acb0b.png"
+  },
+  {
+    "revision": "2efe80c481d49d5b61c189239c927e5a",
+    "url": "/static/media/image 24-1.2efe80c4.png"
+  },
+  {
+    "revision": "b90b4b360cc088b31a3fb2f9297a9df4",
+    "url": "/static/media/image 25.b90b4b36.png"
+  },
+  {
+    "revision": "0936184a113a5552332251408b429730",
+    "url": "/static/media/image 26.0936184a.png"
+  },
+  {
+    "revision": "1f441e3db5f77ba32eb00ff7d76412df",
+    "url": "/static/media/instagram 1.1f441e3d.svg"
+  },
+  {
+    "revision": "906a7b2f488b2672b01d371277c9d14e",
+    "url": "/static/media/linkedin 1.906a7b2f.svg"
+  },
+  {
+    "revision": "83849dfafcdfb571955b987ddfe4e9b5",
+    "url": "/static/media/right-arrow.83849dfa.svg"
+  },
+  {
+    "revision": "eb12e8efc1024249234234d4156b7a07",
+    "url": "/static/media/youtube-logotype 1.eb12e8ef.svg"
+  }
+]);
