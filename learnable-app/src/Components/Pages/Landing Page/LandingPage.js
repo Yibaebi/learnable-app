1b1c45991d916080e88aaa559b94cf8189c28bca
Yibaebi Elliot <59321440+Yibaebi@users.noreply.github.com>
2020-10-20 11:05:21 +0100

    Completed mobile view for landing page

--- learnable-app/src/Components/Pages/Landing Page/LandingPage.js
+++ learnable-app/src/Components/Pages/Landing Page/LandingPage.js
@@ -41,7 +41,7 @@ class LandingPage extends React.Component {
           <header>
             <HamburgerMenu />
           </header>
-          <main className="main-container" id="main-landing-page-container">
+          <main className="main-container mobile-hidden" id="main-landing-page-container">
             <section id="landing-main-body">
              <div id="hero">
                 <section id="hero-intro">
@@ -70,15 +70,6 @@ class LandingPage extends React.Component {
                   />
                 </section>
               </div>
-              {/* <MainSubsection
-                className={className[5]}
-                header={header[0]}
-                text={text[0]}
-                src={require("../../../Images/Landing page/Photo 3.jpg")}
-                alt={alt[0]}
-                buttonTitle={buttonTitle[0]}
-              /> */}
-
               <section className="main-subsection-container">
                 <div className="section-image">
                   <img
@@ -185,6 +176,114 @@ class LandingPage extends React.Component {
                 alt={alt[3]}
                 buttonTitle={buttonTitle[1]}
               />
+            </section>
+            <LandingSubscribe />
+          </main>
+          <main className="main-container desktop-hidden" id="main-landing-page-container" >
+            <section>
+             <div id="hero">
+                <section id="hero-intro">
+                  <h1>
+                    Genesys provides opportunities for young Nigerian techies
+                    and opportunities to grow and innovate.
+                  </h1>
+                  <img
+                    src={require("../../../Mobile Images/Landing First group.png")}
+                    alt={alt}
+                  />
+                  <p>
+                    We understand that opportunities are scarce in these parts.
+                    We are either creating or exposing our people to these
+                    opportunities.
+                  </p>
+                </section>
+              </div>
+              <section className="main-subsection-container">
+                <div className="section-image">
+                  <img
+                    src={require("../../../Mobile Images/Landing group 1/Photo-2.png")}
+                    alt={alt[0]}
+                  />
+                </div>
+                <div className="main-subsection">
+                  <h3>Learnable</h3>
+                  <p>Every year, young Nigerians are launching new careers in the technology industry. Find out how Genesys is helping them do it.</p>
+                  <button id="main-subsection-button">
+                    <a href="#">Learn more</a>
+                  </button>
+                </div>
+              </section>
+              
+              <section className="main-subsection-container">
+                <div className="section-image">
+                  <img
+                    src={require("../../../Mobile Images/Landing group 1/Photo-1.png")}
+                    alt={alt[0]}
+                  />
+                </div>
+                <div className="main-subsection">
+                  <h3>Agora</h3>
+                  <p>We are reimagining the workplace and how people work. Find out how our co-working spaces can help you enjoy the work that you do.</p>
+                  <button id="main-subsection-button">
+                    <a href="#">Learn more</a>
+                  </button>
+                </div>
+              </section>
+              <section className="main-subsection-container">
+                <div className="section-image">
+                  <img
+                    src={require("../../../Mobile Images/Landing group 1/Photo-1.png")}
+                    alt={alt}
+                  />
+                </div>
+                <div className="main-subsection">
+                  <h3>StartZone</h3>
+                  <p>The business environment is harsh for early-stage businesses. We can help you rise above it all.</p>
+                  <button id="main-subsection-button">
+                    <a href="#">Learn more</a>
+                  </button>
+                </div>
+              </section>
+             
+              <section className="service-deliveries-container">
+                <div className="service-deliveries">
+                  <figure id="fig-1">
+                    <img
+                      src={require("../../../Images/Landing page/Rectangle 42.jpg")}
+                      alt="service deliveries"
+                    />
+                    <figcaption>
+                      <a href="/Genesys Ignite">Genesys Ignite</a>
+                    </figcaption>
+                    <img
+                      src={require("../../../Images/Landing page/Rectangle 38.jpg")}
+                      alt="service deliveries"
+                      className="desktop-hidden"
+                    />
+                    <figcaption className="desktop-hidden">
+                      <a href="/CodeVille">CodeVille</a>
+                    </figcaption>
+                    <img
+                      src={require("../../../Images/Landing page/Rectangle 38-1.jpg")}
+                      alt="service deliveries"
+                      className="desktop-hidden"
+                    />
+                    <figcaption className="desktop-hidden">
+                      <a href="/Pink">Pink summer of code</a>
+                    </figcaption>
+                  </figure>
+                </div>
+                <h3>Other things we do</h3>
+              </section>
+              
+              <MainSubsection
+                className="main-subsection-container cadet knowledge"
+                header={header[3]}
+                text={text[3]}
+                src={require("../../../Mobile Images/Landing group 1/Photo.png")}
+                alt={alt[3]}
+                buttonTitle={buttonTitle[1]}
+              /> 
             </section>
             <LandingSubscribe />
           </main>
